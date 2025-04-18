c99dc2874f110e991d50c740e27836f74e1af31d
Mark Krause <110338601+MarkV9@users.noreply.github.com>
2025-04-18 18:10:49 +0200

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,9 @@
+cd ~
+git clone https://github.com/bombela/backward-cpp.git
+#(maybe adjust fork)
+
+sudo apt-get install binutils-dev
+sudo apt-get install libdw-dev
+sudo apt-get install libdwarf-dev
+
+sudo apt-get install libunwind-dev
